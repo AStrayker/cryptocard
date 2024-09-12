ecfc727a4d6516133740be88a88b2f6fb42956ee
AStrayker <astrajker@gmail.com>
2024-09-12 16:40:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-﻿const button = document.querySelector('button');
 
-button.addEventListener('click', () => {
-    // Здесь можно добавить логику для получения карты
-    alert('Ваша карта успішно створена!');
-});
