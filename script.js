221e5453bf5c33db54496c0c814d98091cd28cd8
AStrayker <astrajker@gmail.com>
2024-09-12 16:30:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+﻿const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    // Здесь можно добавить логику для получения карты
+    alert('Ваша карта успішно створена!');
+});
